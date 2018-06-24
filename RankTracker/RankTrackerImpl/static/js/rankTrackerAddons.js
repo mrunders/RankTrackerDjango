0e11e8be8524e10c44d716dbdbcf45b4c4ac52c9
Laurent Thomas <thomas_laurent3@ens.univ-artois.fr>
2018-06-24 19:49:10 +0200

    reset button added on filter

--- RankTracker/RankTrackerImpl/static/js/rankTrackerAddons.js
+++ RankTracker/RankTrackerImpl/static/js/rankTrackerAddons.js
@@ -9,28 +9,36 @@ var switchAcount = function(id){
     $('#id_tab').val(id);
 }
 
-var filter_by_map = function(){
+var filter_merge = function(filtre){
 
-    var filtre = $('#filter_maps_div>#id_map').val() + ':';
     var domtr = document.getElementById('tabletbody');
     var elttr = domtr.childNodes;
 
     elttr.forEach( function(currentValue, currentIndex, listObj){
         currentValue.hidden = (String(currentValue.id).includes(filtre))? false : true;
     });
+}
 
+var filter_by_map = function(){
+
+    var filtre = $('#filter_maps_div>#id_map').val() + ':';
+
+    filter_merge(filtre);
 }
 
 var filter_by_hero = function(){
 
     var filtre = $('#filter_heros_div>#id_heros').val();
-    var domtr = document.getElementById('tabletbody');
-    var elttr = domtr.childNodes;
-
     filtre = $('#filter_heros_div>#id_heros option[value=#{0}]'.replace('#{0}', filtre))[0].innerText;
+    filter_merge(filtre);
+}
 
-    elttr.forEach( function(currentValue, currentIndex, listObj){
-        currentValue.hidden = (String(currentValue.id).includes(filtre))? false : true;
-    });
+var reset_by_hero = function(){
 
+    filter_merge('');
+}
+
+var reset_by_map = function(){
+
+    filter_merge('');
 }
